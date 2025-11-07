8d6e1032df57ad0868259bbd2742a4bad68e7ead
Marian Baba <marianbaba16@gmail.com>
2025-11-07 18:04:43 +0100

    dynamic array class + unit tests

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	testMatch: ["**/*.spec.ts"],
+	moduleNameMapper: {
+		"^@data-structures/(.*)$": "<rootDir>/src/data-structures/$1",
+	},
+};
